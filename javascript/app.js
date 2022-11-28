2e6076de3e903f19a06ab2637b0a5563d0c023b1
tcarrere <tomascarrere4321@gmail.com>
2022-11-27 22:00:06 -0300

    tarea 11.1

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Lunes: Ciclismo 20Km.");
+
+break;
+case "martes":
+
+alert("Martes: Correr 5Km.");
+
+break;
+case "miercoles":
+
+alert("Miércoles: Nadar 2Km.");
+
+break;
+case "jueves":
+
+alert("Jueves: Gimnasio 1Hr.");
+
+break;
+case "viernes":
+
+alert("Viernes: Saltar la cuerda 30min.");
+
+break;
+case "sabado":
+
+alert("Sábado: Caminar 10Km.");
+
+break;
+case "domingo":
+
+alert("Domingo: Descanso.");
+
+break;
+default:
+
+alert("Escribe un día de la semana en minúsculas.");
+}
